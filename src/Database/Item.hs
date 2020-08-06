c45891ac34d8ca3443154f06985e7c95e64a117f
Endi Sukaj <endisukaj@gmail.com>
2020-08-06 15:49:01 +0200

    added is_read to items

--- src/Database/Item.hs
+++ src/Database/Item.hs
@@ -18,23 +18,24 @@ data ItemData = ItemData
     author :: Maybe Text,
     feed_id :: Integer,
     summary :: Maybe Text,
-    description :: Maybe Text
+    description :: Maybe Text,
+    is_read :: Bool
   }
   deriving (Show)
 
 type InsertableItem = (Maybe Text, Maybe Text, Maybe Text, Maybe Text, Integer, Maybe Text, Maybe Text)
 
 instance FromRow ItemData where
-  fromRow = ItemData <$> field <*> field <*> field <*> field <*> field <*> field <*> field <*> field
+  fromRow = ItemData <$> field <*> field <*> field <*> field <*> field <*> field <*> field <*> field <*> field
 
 getAllItems :: Connection -> IO [ItemData]
-getAllItems conn = query_ conn "SELECT * FROM items" :: IO [ItemData]
+getAllItems conn = query_ conn "SELECT * FROM items ORDER BY date_published ASC" :: IO [ItemData]
 
 refreshFeedItems :: Connection -> Integer -> IO ()
 refreshFeedItems conn feedId = do
   (feed : feeds) <- getFeed conn feedId
   items <- readRemoteFeedItems $ feed_url feed
-  executeMany conn "INSERT OR IGNORE INTO items (name, item_url, date_published, author, feed_id, summary, description) VALUES (?, ?, ?, ?, ?, ?, ?)" (Prelude.map ((\a -> a feedId) . tuplefyItem) items)
+  executeMany conn "INSERT OR IGNORE INTO items (name, item_url, date_published, author, feed_id, summary, description, is_read) VALUES (?, ?, ?, ?, ?, ?, ?, 0)" (Prelude.map ((\a -> a feedId) . tuplefyItem) items)
   where
     tuplefyItem :: Item -> Integer -> InsertableItem
     tuplefyItem item feedId = (getItemTitle item, getItemLink item, getItemPublishDateString item, getItemAuthor item, feedId, getItemSummary item, getItemDescription item)
