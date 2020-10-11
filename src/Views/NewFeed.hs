2dccdf9a0382803e877668811ca8ac951b76950a
Endi Sukaj <endisukaj@gmail.com>
2020-10-11 17:38:22 +0200

    improved add new feed page design

--- src/Views/NewFeed.hs
+++ src/Views/NewFeed.hs
@@ -8,13 +8,17 @@ where
 import           Lucid.Base
 import           Lucid.Html5
 import           Views.Mixins.Head              ( pageHead )
+import           Views.Mixins.TopBar            ( topBar )
 
 newFeedView :: Html ()
 newFeedView = html_ $ do
   head_ $ do
     pageHead "Heed - Add New Feed"
   body_ $ do
-    div_ $ do
-      form_ [action_ "/new-feed", method_ "post"] $ do
-        input_ [type_ "text", name_ "feed_url"]
-        input_ [type_ "submit", value_ "Add new feed"]
+    div_ [class_ "container"] $ do
+      topBar
+      div_ [class_ "main center"] $ do
+        form_ [action_ "/new-feed", method_ "post"] $ do
+          h2_ "Add new feed"
+          input_ [type_ "text", name_ "feed_url"]
+          input_ [class_ "button primary", type_ "submit", value_ "Add feed"]
